a70d83bb0e20bcaf14685cae5226b70a547bc751
Brent Langston <blangston@cloudpassage.com>
2019-10-06 02:44:59 -0500

    initial commit

--- bin/grafana.ts
+++ bin/grafana.ts
@@ -3,5 +3,11 @@ import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { GrafanaStack } from '../lib/grafana-stack';
 
+const env = {
+    region: 'us-east-1',
+    account: '651426287273'
+};
+
 const app = new cdk.App();
-new GrafanaStack(app, 'GrafanaStack');
+
+new GrafanaStack(app, 'GrafanaStack', { env });
--- lib/grafana-stack.ts
+++ lib/grafana-stack.ts
@@ -1,9 +1,119 @@
 import cdk = require('@aws-cdk/core');
+import rds = require('@aws-cdk/aws-rds');
+import ec2 = require('@aws-cdk/aws-ec2');
+import ecs = require('@aws-cdk/aws-ecs');
+import { Vpc, IVpc, SubnetType } from '@aws-cdk/aws-ec2';
+import ecs_patterns = require("@aws-cdk/aws-ecs-patterns");
+
+
 
 export class GrafanaStack extends cdk.Stack {
+//  public readonly vpc: IVpc
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+
+// change the below values to fit your environment
+    const dbpassword = "CHANGEME";
+
+    const vpcid = "vpc-0725094f3a85b23ec";
+    const sgip = "10.0.0.0/8";
+
+// end of customization
+
+    const vpc = ec2.Vpc.fromLookup(this, 'vpc', {
+        vpcId: vpcid
+    });
+
+    const privatesubnets = vpc.selectSubnets({
+         subnetType: SubnetType.PRIVATE
+    })
+
+//    this.vpc = Vpc.fromVpcAttributes(this, "Vpc", {
+//        vpcId: vpcid,
+//        availabilityZones: availabilityzones,
+//        privateSubnetIds: privatesubnets,
+//        publicSubnetIds: publicsubnets
+//    })
+
+    const rdsdbsubnets = new rds.CfnDBSubnetGroup(this, 'rdsdbsubnets', {
+        dbSubnetGroupDescription: "grafana rds db subnet group",
+        dbSubnetGroupName: "grafana-db-subnet-group",
+        subnetIds: privatesubnets.subnetIds
+    });
+
+    const dbsecuritygroup = new ec2.CfnSecurityGroup(this, 'dbsecuritygroup', {
+        groupDescription: "grafana db security group",
+        groupName: "grafana-db-sg",
+        vpcId: vpcid,
+        securityGroupIngress: [
+            {
+                cidrIp: sgip,
+                fromPort: 3306,
+                ipProtocol: "tcp",
+                toPort: 3306
+            }
+        ],
+        securityGroupEgress: [
+            {
+                cidrIp: "0.0.0.0/0",
+                ipProtocol: "-1"
+            }
+        ]
+    });
+
+    const rdsdb = new rds.CfnDBCluster(this, 'rdsdb', {
+        availabilityZones: vpc.availabilityZones,
+        backupRetentionPeriod: 7,
+        dbClusterIdentifier: "grafana-db",
+        // dbSubnetGroupName: "grafana-db-subnet-group",
+        dbSubnetGroupName: rdsdbsubnets.dbSubnetGroupName,
+        engine: "aurora",
+        port: 3306,
+        masterUsername: "admin",
+        masterUserPassword: dbpassword,
+        vpcSecurityGroupIds: [
+            dbsecuritygroup.ref
+        ],
+        storageEncrypted: true,
+        engineMode: "serverless",
+        deletionProtection: false
+    });
+
+    const cluster = new ecs.Cluster(this, 'grafanacluster', {
+        vpc: vpc,
+        clusterName: 'grafana'
+    });
+
+    const taskdef = new ecs.FargateTaskDefinition(this, 'taskdef', {
+        cpu: 256,
+        memoryLimitMiB: 512,
+    });
+
+    const container = taskdef.addContainer("grafana", {
+        image: ecs.ContainerImage.fromRegistry("grafana/grafana"),
+        environment: {
+            TEST_ENVIRONMENT_VARIABLE: "test-value",
+            GF_INSTALL_PLUGINS: "grafana-clock-panel,grafana-simple-json-datasource",
+            GF_DATABASE_TYPE: rdsdb.engine,
+            GF_DATABASE_PASSWORD: String(rdsdb.masterUserPassword),
+            GF_DATABASE_USER: String(rdsdb.masterUsername),
+            GF_DATABASE_HOST: rdsdb.attrEndpointAddress,
+            GF_SECURITY_ADMIN_PASSWORD: dbpassword,
+            GF_AWS_default_REGION: this.region
+        }
+    });
+
+    container.addPortMappings({
+        containerPort: 3000
+    })
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "grafanaservice", {
+        cluster: cluster,
+        taskDefinition: taskdef,
+        serviceName: 'grafana',
+        publicLoadBalancer: true
+    });
+
   }
 }
